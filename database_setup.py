292e9199c43a077b4c7d2f1ad52c858f0d748adf
Candy_Tsai <candy02058912@gmail.com>
2018-08-16 00:14:37 +0800

    setup database schema

---
+++ database_setup.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    picture = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'picture': self.price,
+        }
+
+
+engine = create_engine('sqlite:///stuffedanimals.db')
+Base.metadata.create_all(engine)
